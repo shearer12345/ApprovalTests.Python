d47095186b102ea51059f68770a49638a84ca949
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-08-28 17:43:58 +0000

    *** wip - working on git diff reporting
    Co-Authored-By: 4dsherwood <4dsherwood@users.noreply.github.com>
    Co-Authored-By: Gregor Riegler <rieglerg85@gmail.com>
    Co-Authored-By: Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
    Co-Authored-By: Eva Nanyonga <evewish@gmail.com>
    Co-Authored-By: Jay Bazuzi <jay@bazuzi.com>
    Co-Authored-By: Nitsan Avni <nitsanav@gmail.com>
    Co-Authored-By: Susan Fung <38925660+susanfung@users.noreply.github.com>

---
+++ tests/reporters/GitDiffTest.py
@@ -0,0 +1,19 @@
+from approvaltests import Options, verify, Reporter
+from approvaltests.reporters import ReportWithBeyondCompare, GenericDiffReporter, GenericDiffReporterConfig, \
+    create_config
+
+
+class ReportWithGitDiff(Reporter):
+    def report(self, received_path: str, approved_path: str) -> bool:
+        # call `git diff` on these paths
+        #   def __init__(self, name: str, path: str, extra_args: Optional[List[str]] = None):
+        return GenericDiffReporter(GenericDiffReporterConfig(
+            name="gitdiff",
+            path="c:/program files/git/usr/bin/diff.exe",
+            extra_args=["--color", "-u"],
+        )).report(received_path, approved_path)
+
+
+
+def test_something():
+    verify("applesauce", options=Options().with_reporter(ReportWithGitDiff()))
